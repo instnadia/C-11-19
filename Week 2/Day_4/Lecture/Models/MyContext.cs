ffe629ec51dca7d38e4e29a3bcbe69e97bfe9276
instnadia <nnguyen@codingdojo.com>
2019-12-05 11:09:42 -0800

    log and reg with hash

---
+++ Week 2/Day_4/Lecture/Models/MyContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Lecture.Models
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions options) : base(options) { }
+        public DbSet<User> Users {get;set;}
+    }
+}
